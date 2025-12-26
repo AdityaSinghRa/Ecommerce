d4a5ba68c5b29353625c5531a675649250b00a6b
AdityaSinghRa <aditya2331110@akgec.ac.in>
2025-12-26 13:27:36 +0530

    email error

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,19 +1,23 @@
-import nodeMailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-export const sendEmail=async(options)=>{
-  const transporter=nodeMailer.createTransport({
-    service:process.env.SMTP_SERVICE,
-    auth:{
-      user:process.env.SMTP_MAIL,
-      pass:process.env.SMTP_PASSWORD
-    }
-  })
-const mailOptions={
-  from:process.env.SMTP_MAIL,
-  to:options.email,
-  subject:options.subject,
-  text:options.message
-}
+export const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-await transporter.sendMail(mailOptions);
-}
+  const mailOptions = {
+    from: `"Support Team" <${process.env.SMTP_MAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
